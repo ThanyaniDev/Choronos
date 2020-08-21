268f86657fa345ad8634a30d90ed451922d53300
ThanyaniTshikonwani <thanyani.tshikonwani@umuzi.org>
2020-08-21 18:39:40 +0200

    Initial Commit

---
+++ Chronos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chronos
+//
+//  Created by Thanyani on 2020/08/21.
+//  Copyright © 2020 Thanyani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
